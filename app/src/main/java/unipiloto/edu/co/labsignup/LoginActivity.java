0590efd52b2e77292e66a3816b1ebe7b68dc1bf6
JuanMesa527 <mesacalderon@gmail.com>
2024-09-22 17:08:18 -0500

    final

--- app/src/main/java/unipiloto/edu/co/labsignup/LoginActivity.java
+++ app/src/main/java/unipiloto/edu/co/labsignup/LoginActivity.java
@@ -2,6 +2,7 @@ package unipiloto.edu.co.labsignup;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,19 +10,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    public void registro(View view) {
         Intent intent = new Intent(this, SignupActivity.class);
         startActivity(intent);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
--- app/src/main/java/unipiloto/edu/co/labsignup/SignupActivity.java
+++ app/src/main/java/unipiloto/edu/co/labsignup/SignupActivity.java
@@ -1,5 +1,6 @@
 package unipiloto.edu.co.labsignup;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import android.Manifest;
@@ -8,15 +9,20 @@ import android.os.Bundle;
 import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -24,34 +30,32 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
+import java.util.Date;
+
 public class SignupActivity extends AppCompatActivity {
     public static final int REQUEST_CODE = 1;
-    EditText lat, lon, dir;
-    Button obtener, salida;
+    TextView lat, lon;
+    Button obtener;
+    EditText editTextDate;
 
     FusedLocationProviderClient fusedLocationProviderClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
         lat = findViewById(R.id.edtlatitud);
         lon = findViewById(R.id.edtlongitud);
+        editTextDate = findViewById(R.id.TextDate);
 
         obtener = findViewById(R.id.button);
-        //salida = findViewById(R.id.btnSalir);
-
-        //progressBar = findViewById(R.id.progressBar);
-        //fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
     public void ObtenerCoordendasActual(View view) {
-
-
         if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
             ActivityCompat.requestPermissions(SignupActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
         } else {
-
             getCoordenada();
         }
     }
@@ -69,15 +73,12 @@ public class SignupActivity extends AppCompatActivity {
     }
 
     private void getCoordenada() {
-
         try {
-
             LocationRequest locationRequest = new LocationRequest();
             locationRequest.setInterval(10000);
             locationRequest.setFastestInterval(3000);
             locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
                 return;
             }
             LocationServices.getFusedLocationProviderClient(this).requestLocationUpdates(locationRequest, new LocationCallback() {
@@ -95,13 +96,26 @@ public class SignupActivity extends AppCompatActivity {
                 }
 
             }, Looper.myLooper());
-
         }catch (Exception ex){
             System.out.println("Error es :" + ex);
         }
-
     }
-    public  void Exit(View view){
-        this.finish();
+
+    public void openDatePicker(View view) {
+        Date date = new Date();
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                int currentYear = date.getYear() + 1900;
+                int age = currentYear - year;
+                if (age < 18 || (age == 18 && (month > date.getMonth() || (month == date.getMonth() && day > date.getDate())))) {
+                    Toast.makeText(SignupActivity.this, "Debes ser mayor de 18 años", Toast.LENGTH_SHORT).show();
+                } else {
+                    editTextDate.setText(String.valueOf(day) + "/" + String.valueOf(month + 1) + "/" + String.valueOf(year));
+                }
+            }
+        }, (date.getYear() + 1900), (date.getMonth()), (date.getDate()));
+
+        datePickerDialog.show();
     }
 }
